de3000ddc064c3bfab2c13a9d09d34a5c5a8c274
dpanait6 <119539523+dpanait6@users.noreply.github.com>
2023-06-27 17:39:19 +0200

    final

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const puppeteer = require('puppeteer');
 app.disableHardwareAcceleration();
 app.commandLine.appendSwitch("xhttp.htmlHandler: xhttp.convertSync: printer.chromePrinter.Print.--no-sandbox.--disable-dev-shm-usage");
 let install_path = process.env.TEMP;
-devToolsLogJSON(install_path);
+//devToolsLogJSON(install_path);
 //console.log(process);
 let url_to_open = "";
 const url_base = `file://${__dirname}\\index.html`;
@@ -85,10 +85,11 @@ const open_url_new_window = (mainWindow, url, print_tik, print_docs, print_trans
 	//url = "https://yuubbb.com/pre/dani/iluminashop/impresion/ticket_TM4T2?tok=94c48b5d5f59cf93c3c7add6a1b64d30";//&ml=2&mr=7
 	//url = "https://yuubbb.com/pre/dani/iluminashop/impresion/ticket_TM6T3?tok=94c48b5d5f59cf93c3c7add6a1b64d30"
 	//url = "https://yuubbb.com/pre/dani/iluminashop/impresion/factura1_T1T2Z"
-	//url = "https://yuubbb.com/pre/dani/ledmeeuropa/impresion/albaran_seur_TMPT3?SEUR=TMPT320230424182552&transport=1"
+	//url = "https://yuubbb.com/pre/dani/iluminashop/impresion/albaran_seur_TM64K?SEUR=TM64K20230426110211&transport=1"
+	//url = "https://yuubbb.com/pre/dani/iluminashop/impresion/albaran_envialia_TM642?ENVIALIA=TM642_20230426103923"
 	
-	devToolsLogJSON(mainWindow, url);
-	devToolsLogJSON(mainWindow, __dirname);
+	//devToolsLogJSON(mainWindow, url);
+	//devToolsLogJSON(mainWindow, __dirname);
 
 	let printer_name = "EPSON_SX235_Series";
 	let path_pdf = `${install_path}${path.sep}doc.pdf`;
@@ -97,7 +98,8 @@ const open_url_new_window = (mainWindow, url, print_tik, print_docs, print_trans
 	let num_copy = 1;
 	let params_url = "";
 	let margin = {};
-	let width = 0;
+	var width = 0;
+	var ticket_width = 280;
 
 	//print_docs = true;
 	//print_tik = true;
@@ -112,13 +114,13 @@ const open_url_new_window = (mainWindow, url, print_tik, print_docs, print_trans
 		let bottom = store.get("tickets_bottom") != undefined || store.get('tickets_bottom') != 0 ? store.get('tickets_bottom') : 0;
 		num_copy = store.get("tickets_copy") != undefined || store.get('tickets_copy') != 0 ? store.get('tickets_copy') : 1;
 		margin = {
-			top: parseInt(top) + 'mm',			
-			right: parseInt(right) + 'mm',			
-			bottom:parseInt(bottom) + 'mm',
-			left: parseInt(left)+ 'mm'
+			top: parseInt(top) ,			
+			right: parseInt(right),			
+			bottom: parseInt(bottom),
+			left: parseInt(left)
 		}
-		params_url = `&ml=${left}&mr=${right}&mt=${top}&mb=${bottom}`;
-		devToolsLogJSON(mainWindow, margin);
+		//params_url = `&ml=${left}&mr=${right}&mt=${top}&mb=${bottom}`;
+		//devToolsLogJSON(mainWindow, margin);
 		
 		printer_name = store.get('ticket_printer');
 		if(printer_name == undefined || printer_name == 0){
@@ -132,20 +134,20 @@ const open_url_new_window = (mainWindow, url, print_tik, print_docs, print_trans
 	if(print_docs){
 		
 		//url_to_print = "https://yuubbb.com/pre/dani/iluminashop/impresion/factura1_T1T2Z";
-		width = 860;
+		width = store.get("docs_width") != undefined || store.get('docs_width') != 0 ? store.get('docs_width') : 860;
 		let top = store.get("docs_top") != undefined || store.get('docs_top') != 0 ? store.get('docs_top') : 2;
 		let left = store.get("docs_left") != undefined || store.get('docs_left') != 0 ? store.get('docs_left') : 2;
 		let right = store.get("docs_right") != undefined || store.get('docs_right') != 0 ? store.get('docs_right') : 2;
 		let bottom = store.get("docs_bottom") != undefined || store.get('docs_bottom') != 0 ? store.get('docs_bottom') : 2;
 		num_copy = store.get("docs_copy") != undefined || store.get('docs_copy') != 0 ? store.get('docs_copy') : 1;
 		margin = {
-			top: top+'px',
-			right: right+'px',
-			bottom: bottom+'px',
-			left: left+'px'
+			top: top,
+			right: right,
+			bottom: bottom,
+			left: left
 		}
 		/*pdf_options.margin = margin;//ponemos los margenes de la impresion*/
-		params_url = `&ml=${left}&mr=${right}&mt=${top}&mb=${bottom}`;
+		//params_url = `&ml=${left}&mr=${right}&mt=${top}&mb=${bottom}`;
 		printer_name = store.get('docs_printer');
 		if(printer_name == undefined || printer_name == 0){
         
@@ -156,7 +158,7 @@ const open_url_new_window = (mainWindow, url, print_tik, print_docs, print_trans
 	}
 	if(print_transport){
 		
-		width = 700;
+		width = store.get("transport_width") != undefined || store.get('transport_width') != 0 ? store.get('transport_width') : 700;
 		let top = store.get("transport_top") != undefined || store.get('transport_top') != 0 ? store.get('transport_top') : 2;
 		let left = store.get("transport_left") != undefined || store.get('transport_left') != 0 ? store.get('transport_left') : 2;
 		let right = store.get("transport_right") != undefined || store.get('transport_right') != 0 ? store.get('transport_right') : 2;
@@ -169,7 +171,7 @@ const open_url_new_window = (mainWindow, url, print_tik, print_docs, print_trans
 			left: left			
 		}
 		/*pdf_options.margin = margin;//ponemos los margenes de la impresion*/
-		params_url = `&ml=${left}&mr=${right}&mt=${top}&mb=${bottom}`;
+		//params_url = `&ml=${left}&mr=${right}&mt=${top}&mb=${bottom}`;
 		printer_name = store.get('transport_printer');
 		if(printer_name == undefined || printer_name == 0){
         
@@ -186,9 +188,9 @@ const open_url_new_window = (mainWindow, url, print_tik, print_docs, print_trans
 	//devToolsLogJSON(mainWindow, `command: ${command}`);
 	//console.log(pdf_options)
 	//params_url = '&ml=2&mr=3&mt=0&mb=0';
-	console.log(url + params_url)
+	//console.log(url + params_url)
 	
-	mainWindow.loadURL(url /*+ params_url*/);
+	mainWindow.loadURL(url /*+params_url*/);
 	// abrimos la url en una nueva ventana
 	mainWindow.webContents.on("dom-ready", async function(){
 		let printers = await mainWindow.webContents.getPrintersAsync();
@@ -202,30 +204,63 @@ const open_url_new_window = (mainWindow, url, print_tik, print_docs, print_trans
 			});
   			const page = await browser.newPage();
 			//await page.setViewport({width: 280, height: 3500});
-			await page.goto(url + params_url);
+			await page.goto(url /*+ params_url*/);
 			//await page.setContent(html);
 			
-			await page.content();
+			//await page.content();
 			heightContainer = await page.evaluate(() => document.documentElement.offsetHeight);
-			//await page.evaluate(() => document.getElementById('container_ticket').style.width = '360px');
-			console.log(heightContainer);
+			//
+			//console.log("heightContainer", heightContainer);
+			//console.log("width", width);
 			if(print_tik){
-				heightContainer = heightContainer / (1 + 30 / 100);
+				if(heightContainer > 1000){
+					if(width > 280) {
+						heightContainer = heightContainer / (1 - 5 / 100)
+					} else {
+						heightContainer = heightContainer / (1 + 25 / 100);
+					}
+					
+				}
+				
+				//await page.evaluate((width, left, right) => document.getElementById('container_ticket').offsetWidth = width + left + right);
+				//let width_cont = await page.evaluate((width, left, right) => document.getElementById('container_ticket').offsetWidth);
+				//console.log("width_cont", width_cont);
+			}
+			if(print_transport){
+				heightContainer = heightContainer / (1 + 14 / 100);
+			}
+			if(print_docs){
+				heightContainer = heightContainer / (1 - 5 / 100);
+			}
+			if(print_docs || print_transport){
+				// albaran transporte
+				let transport_print_button = await page.evaluate(()=> document.getElementById('options_view'));
+				if(transport_print_button){
+					await page.evaluate(()=> document.getElementById('options_view').style.display = 'none');
+				}
+				// facturas
+				let docs_print_button = await page.evaluate(()=> document.getElementById('butons'));
+				if(docs_print_button){
+					await page.evaluate(()=> document.getElementById('butons').style.display = 'none');
+				}
+				
 			}
 			
-			
-			await page.evaluate(()=> document.getElementsByClassName('buttons')[0].style.display = 'none');
 			/*let buttons = await page.waitForSelector(" div > .buttons");
 			await buttons.style.display = 'none';*/
-			console.log(heightContainer);
+			//console.log(heightContainer);
 			// Set screen size
+			//await page.emulate('screen');
+			// emulamos una pantalla para poder poner magenes
+			await page.emulateMediaType("screen");
+		
  			const pdfOptions = {
 				path: path_pdf,
 				width: width + 'px',
 				height: heightContainer+'px',
 				margin: margin,//{ top: '5mm', right: '3mm', bottom: '8mm', left: '0mm' },
-				printBackground: true 
-				///preferCSSPageSize: false
+				/*printBackground: true ,
+				preferCSSPageSize: true*/
 			} 
 			await page.pdf(pdfOptions);
 			
@@ -239,11 +274,11 @@ const open_url_new_window = (mainWindow, url, print_tik, print_docs, print_trans
 				copies: parseInt(num_copy),
 				margin: margin
 			};
-			console.log("printer options", options)
-			print(path_pdf, options).then(response => {
+			//console.log("printer options", options)
+			/*print(path_pdf, options).then(response => {
 				//console.log(response);
-			});
-			/*exec(command, (error, stdout, stderr) => {
+			});*/
+			exec(command, (error, stdout, stderr) => {
 				if (error) {
 					devToolsLogJSON(mainWindow, `error: ${error.message}`);
 					console.log(`error: ${error.message}`);
@@ -256,7 +291,7 @@ const open_url_new_window = (mainWindow, url, print_tik, print_docs, print_trans
 				}
 				devToolsLogJSON(mainWindow, `stdout: ${stdout}`);
 				console.log(`stdout: ${stdout}`);
-			});*/
+			});
 		
 		}
 	});
